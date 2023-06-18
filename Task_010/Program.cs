139488483b24e9b7f620d8f722028d01a94eb97f
SashaSmormova <zorrogirl@mail.ru>
2023-06-18 11:05:44 +0300

    решение второй 10 задачи

--- Task_010/Program.cs
+++ Task_010/Program.cs
@@ -2,3 +2,19 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+int number = 0;
+Console.WriteLine("Введите трехзначное число");
+
+number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    number = number / 10;
+    number = number % 10;
+}
+else 
+{
+    System.Console.WriteLine("Вы ввели некорректное число. Попробуйте ещё раз: ");
+}
+
+Console.WriteLine("Вторая цифра: " + number);
