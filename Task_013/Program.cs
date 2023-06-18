cd4a61f3b0bff9902f173817dea2f2d664aff801
SashaSmormova <zorrogirl@mail.ru>
2023-06-18 11:27:40 +0300

    решила 13 задачу

--- Task_013/Program.cs
+++ Task_013/Program.cs
@@ -2,3 +2,28 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 100 || number < -100)
+{
+    while (number > 1000 || number < -1000)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    if (number < 0)
+    {
+        number = number * -1;
+        System.Console.WriteLine("Третья цифра:" + number);
+    }
+    else 
+    {
+        Console.WriteLine("Третья цифра:" + number);
+    }   
+}
+else
+{
+    System.Console.WriteLine("В этом' числе нет третьей цифры");
+} 
