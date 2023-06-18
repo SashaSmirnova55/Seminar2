0c4137a038680713e59b5174b9764bac15aa042f
SashaSmormova <zorrogirl@mail.ru>
2023-06-18 11:38:42 +0300

    все задачи готовы

--- Task_015/Program.cs
+++ Task_015/Program.cs
@@ -2,3 +2,32 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+int day = ReadInt("Введите число от 1 до 7, например понедельник 1: ");
+Console.WriteLine(DayOfTheWeek(day));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string DayOfTheWeek(int day)
+{
+    if (day > 0 && day < 8)
+    {
+        if (day == 7 || day == 6)
+        {
+            Console.Write("Выходной ");
+        }
+        else
+        {
+            Console.Write("Рабочий ");
+        }
+    }
+    else
+    {
+        Console.Write("Неверное число");
+    }
+    return "день";
+}
